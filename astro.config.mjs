e3def97c37c270a463b50f2f08a73b0d3c167b8d
DaviWebDev <davicarvalho401@gmail.com>
2026-09-10 21:16:40 -0300

    chore: adiciona configuração básica do site, variáveis padrões, reset e logos de sites. Além da adição do componente head.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,3 @@
-import { defineConfig, fontProviders } from "astro/config";
+import { defineConfig } from "astro/config";
 
-export default defineConfig({
-  fonts: [
-    {
-      provider: fontProviders.local(),
-      name: "urbanistLatin",
-      cssVariable: "--font-urbanist-latin",
-      options: {
-        variants: [
-          {
-            src: ["./src/assets/fonts/urbanist-latin-400-normal.woff2"],
-            weight: 400,
-            style: "normal",
-          },
-          {
-            src: ["./src/assets/fonts/urbanist-latin-600-normal.woff2"],
-            weight: 600,
-            style: "normal",
-          },
-          {
-            src: ["./src/assets/fonts/urbanist-latin-800-normal.woff2"],
-            weight: 800,
-            style: "normal",
-          },
-        ],
-      },
-    },
-  ],
-});
+export default defineConfig({});
